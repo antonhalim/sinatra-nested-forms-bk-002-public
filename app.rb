67b37113b904bf549d87cfa6b5eec6addbf53290
axton21 <axton_n@yahoo.com>
2015-03-20 00:53:06 -0400

    pirates

--- app.rb
+++ app.rb
@@ -2,6 +2,21 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    # define your routes here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = params["pirate"]
+      @ship0 = params["ship"]["0"]
+      @ship1 = params["ship"]["1"]
+      erb :show
+
+    end
+
   end
 end
--- environment.rb
+++ environment.rb
@@ -2,7 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+require 'pry'
 require 'sinatra/base'
 require 'sinatra/reloader'
 
